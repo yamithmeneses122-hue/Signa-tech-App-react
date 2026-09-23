c16f02e4ff5165aa0a3b67561db40f6a79b4aa1c
yamith_1 <yamithmeneses122@gmail.com>
2026-09-23 17:00:32 -0500

    Refactor interpreter semantic structure: src/Features/Interpreter/Componentes/component.jsx

--- src/Features/Interpreter/Componentes/component.jsx
+++ src/Features/Interpreter/Componentes/component.jsx
@@ -1,4 +1,10 @@
-export { default as Sidebar } from "./Sidebar";
-export { default as PageHeader } from "./PageHeader";
-export { default as StatCard } from "./StatCard";
+export { default as Filter } from "./Filter";
+export { default as Header } from "./Header";
 export { default as InterpreterLayout } from "./InterpreterLayout";
+export { default as PageHeader } from "./PageHeader";
+export { default as Pagination } from "./Pagination";
+export { default as SearchBar } from "./SearchBar";
+export { default as Sidebar } from "./Sidebar";
+export { default as SignCard } from "./SignCard";
+export { default as StatCard } from "./StatCard";
+export { default as StatusBadge } from "./StatusBadge";
