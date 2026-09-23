6d27443553d4d6ec1b3b7aa1c4c74e966f5b0045
yamith_1 <yamithmeneses122@gmail.com>
2026-09-23 15:06:55 -0500

    feat(interpreter): complete remaining views

---
+++ src/Features/Interpreter/Vistas/vista.jsx
@@ -0,0 +1,7 @@
+export { default as Interprete } from "./Interprete";
+export { default as Validar } from "./Validar";
+export { default as Registrar } from "./Registrar";
+export { default as Corregir } from "./Corregir";
+export { default as Historial } from "./Historial";
+export { default as Estadisticas } from "./Estadisticas";
+export { default as Perfil } from "./Perfil";
