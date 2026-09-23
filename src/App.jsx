3f0ce8343380bc75209c742e533e14efded722c9
yamith_1 <yamithmeneses122@gmail.com>
2026-09-23 15:07:37 -0500

    feat(interpreter): connect interpreter routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,26 @@ import HeaderProductoLayout from "./Routes/HeaderProducto";
 import HomeView from "./Features/Public/view/Homeview";
 import ProductView from "./Features/Public/view/Productview";
 import SupportView from "./Features/Public/view/SupportView";
+import { InterpreterRoutes } from "./Features/Interpreter/InterpreterRoutes";
 
 function App() {
     return (
         <BrowserRouter>
-
             <Routes>
-
                 <Route element={<HeaderHomeLayout />}>
-
-                    <Route
-                        path="/"
-                        element={<HomeView />}
-                    />
-
-                    <Route
-                        path="/home"
-                        element={<HomeView />}
-                    />
-
+                    <Route path="/" element={<HomeView />} />
+                    <Route path="/home" element={<HomeView />} />
                 </Route>
 
                 <Route element={<HeaderProductoLayout />}>
-
-                    <Route
-                        path="/product"
-                        element={<ProductView />}
-                    />
-
-                    <Route
-                        path="/support"
-                        element={<SupportView />}
-                    />
-
+                    <Route path="/product" element={<ProductView />} />
+                    <Route path="/support" element={<SupportView />} />
                 </Route>
 
-                <Route
-                    path="*"
-                    element={<Navigate to="/" replace />}
-                />
+                {InterpreterRoutes()}
 
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
-
         </BrowserRouter>
     );
 }
